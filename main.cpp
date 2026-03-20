1101d509ffcea7144988bde6f29b457ec663375d
root <root@Aakarsh.localdomain>
2026-03-20 20:04:22 +0530

    feat: added memory usage percentage calculation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -8,22 +9,46 @@ int main() {
     ifstream memFile("/proc/meminfo");
 
     if (!memFile.is_open()) {
-        cout << "Error: Could not open /proc/meminfo. Are you sure you are on Linux?" << endl;
+        cout << "Error: Could not open /proc/meminfo." << endl;
         return 1;
     }
 
     string line;
+    long long totalMem = 0;
+    long long availableMem = 0;
     
     while (getline(memFile, line)) {
         if (line.find("MemTotal:") == 0) {
-            cout << "Raw OS Data -> " << line << endl;
+            string label;
+            long long value;
+            string unit;
+            
+            stringstream ss(line);
+            ss >> label >> value >> unit; 
+            totalMem = value;
         }
+        
         if (line.find("MemAvailable:") == 0) {
-            cout << "Raw OS Data -> " << line << endl;
+            string label;
+            long long value;
+            string unit;
+            
+            stringstream ss(line);
+            ss >> label >> value >> unit;
+            availableMem = value;
         }
     }
 
     memFile.close();
+
+    if (totalMem > 0) {
+        long long usedMem = totalMem - availableMem;
+        double memUsagePercent = (static_cast<double>(usedMem) / totalMem) * 100.0;
+        
+        cout << "System Memory Usage: " << memUsagePercent << "%" << endl;
+    } else {
+        cout << "Failed to parse memory." << endl;
+    }
     
     return 0;
 }
